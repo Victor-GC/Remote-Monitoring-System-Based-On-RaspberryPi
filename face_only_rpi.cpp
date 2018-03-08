860c7e496de46a95f4a16c46a91a8d105c6cef58
Jeffrey-Jiang <chloroplast@126.com>
2018-03-08 14:59:46 +0800

    add save and delete .jpg

--- face_only_rpi.cpp
+++ face_only_rpi.cpp
@@ -6,39 +6,72 @@
 using namespace cv;
 using namespace std;
 
-void detectAndDraw( Mat& img, CascadeClassifier& cascade,
-                  // CascadeClassifier& nestedCascade,
-                   double scale, bool tryflip );
+int detectAndDraw( Mat& img, CascadeClassifier& cascade, double scale, bool tryflip );
 
 int main()
-{
-    VideoCapture cap(0);    //打开树莓派摄像头
-    if(!cap.isOpened())
+{	
+    /*变量参数准备*/
+	Mat frame; //用于保存摄像头采集得到的图片
+	int number_of_face = 0;  //识别出的人脸数量
+    CascadeClassifier cascade; //分类器
+
+    //加载已训练好的分类模型，注意路径修改
+    cascade.load("/home/pi/opencv-3.4.0/data/haarcascades/haarcascade_frontalface_alt.xml");
+    
+	//打开树莓派摄像头
+	VideoCapture cap(0);    
+    if (!cap.isOpened())
     {
         return -1;
     }
 	
-    Mat frame;
-    //Mat edges;
-
-    CascadeClassifier cascade, nestedCascade;
-    bool stop = false;
-    //训练好的文件名称，放置在可执行文件同目录下
-    cascade.load("/home/pi/opencv-3.4.0/data/haarcascades/haarcascade_frontalface_alt.xml");
-    //nestedCascade.load("/home/jeffrey/test/opencv-3.4.0/data/haarcascades/haarcascade_eye.xml");
-    
-	//frame = imread("/home/pi/opencv-3.4.0/demo/face_only/lena.jpg");
-    cap>>frame;//读取摄像头的当前帧
+    while (1)
+	{
+		//从本地读取摄像头图片 调试使用
+		//frame = imread("/home/pi/opencv-3.4.0/demo/face_only/lena.jpg");
+		//读取摄像头的当前帧
+		cap >> frame;
+		
+		if (frame.empty())
+			cout<<"Fail to capture frame!"<<endl;
+		else
+			imwrite("test.jpg", frame); //保存当前摄像头捕捉到的图片至当前文件夹下
+		
+		//进行人脸检测
+		number_of_face = detectAndDraw( frame, cascade, 2, 0 );
+		
+		if (number_of_face >= 1)
+		{
+			cout<<"Attention!"<<endl;
+			/*触发报警部分功能*/
+			
+		}
+		else
+		{
+			cout<<"Don't worry"<<endl;
+		}
+		
+		//删除保存的图片
+		int result_delete = remove("./test.jpg");
+		if (!result_delete)
+			cout<<"delete succeeded"<<endl;
+		else
+			cout<<"delete failed"<<endl;
+		
+		//27是键盘按下esc时，计算机收到的ascii码值，waitKey(30)表示等待30ms
+		//在实际运行过程中，控制台是无法接受到waitkey的键值的，只有在显示的图像上才能收到键值
+		/*if (waitKey(30)==27)
+			break;
+		*/
+		
+		//延时1000ms，用于控制监测实时性
+		waitKey(1000);
+	}
 	
-	detectAndDraw( frame, cascade,/* nestedCascade,*/2,0 );
-    
-	//waitKey(0);
-    
     return 0;
 }
-void detectAndDraw( Mat& img, CascadeClassifier& cascade,
-                  // CascadeClassifier& nestedCascade,
-                   double scale, bool tryflip )
+
+int detectAndDraw( Mat& img, CascadeClassifier& cascade, double scale, bool tryflip )
 {
     int i = 0;
     double t = 0;
@@ -98,6 +131,7 @@ void detectAndDraw( Mat& img, CascadeClassifier& cascade,
     t = (double)cvGetTickCount() - t;
     printf( "detection time = %g ms\n", t/((double)cvGetTickFrequency()*1000.) );
     
+	//用于在图片上标记出人脸位置
     for( vector<Rect>::const_iterator r = faces.begin(); r != faces.end(); r++, i++ )
     {
         Mat smallImgROI;
@@ -121,26 +155,9 @@ void detectAndDraw( Mat& img, CascadeClassifier& cascade,
             rectangle( img, cvPoint(cvRound(r->x*scale), cvRound(r->y*scale)),
             cvPoint(cvRound((r->x + r->width-1)*scale), cvRound((r->y + r->height-1)*scale)),
             color, 3, 8, 0);
-       /*
-        if( nestedCascade.empty() )
-            continue;
-        smallImgROI = smallImg(*r);
-        //同样方法检测人眼
-        nestedCascade.detectMultiScale( smallImgROI, nestedObjects,
-            1.1, 2, 0
-            //|CV_HAAR_FIND_BIGGEST_OBJECT
-            //|CV_HAAR_DO_ROUGH_SEARCH
-            //|CV_HAAR_DO_CANNY_PRUNING
-            |CV_HAAR_SCALE_IMAGE
-            ,Size(30, 30) );
-        for( vector<Rect>::const_iterator nr = nestedObjects.begin(); nr != nestedObjects.end(); nr++ )
-        {
-            center.x = cvRound((r->x + nr->x + nr->width*0.5)*scale);
-            center.y = cvRound((r->y + nr->y + nr->height*0.5)*scale);
-            radius = cvRound((nr->width + nr->height)*0.25*scale);
-            circle( img, center, radius, color, 3, 8, 0 );
-        }*/
     }
     //imshow( "识别结果", img );
+	//调试使用，输出检测到的人脸数量
     cout<<"The number of detected faces is "<<faces.size()<<endl;
+	return faces.size();
 }
