d07c095a5d01e8ff5262595515c02b4d6b22f1f9
mylove-chloe <gengchao23@163.com>
2018-03-11 10:45:33 +0800

    新建短信功能开发分支

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include "SMSsending.h"
 
-/*全局变量*/
-int alarm_index = 1;//报警标志位,值为1则报警
 
 int main(int argc, char *argv[])
 {
@@ -13,23 +11,13 @@ int main(int argc, char *argv[])
         return 0;
     }
 
+    wchar_t* phone_num = L"15765545478";
+    wchar_t* send_data = L"测试一下，看行不行！";
 
-    while(1)
+    if(!send_SMS(phone_num, send_data))
     {
-        /*TODO 添加摄像头检测*/
-
-        /*短信报警部分*/
-        if(alarm_index)//需要报警
-        {
-            wchar_t* phone_num = L"15765545478";
-            wchar_t* send_data = L"测试一下，看行不行！";
-
-            if(!send_SMS(phone_num, send_data))
-            {
-                printf("发送短信失败！\n");
-            }
-            break;
-        }
+        printf("发送短信失败！\n");
     }
+    
     return 0;
 }
