12bebca9bc131939d382b8e3234227cce99f9034
Victor Geng <gengchao23@163.com>
2018-03-23 09:29:18 +0000

    合并

---
+++ httpserver/CMakeFiles/RemoteMonit.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/Desktop/Remote-Monitoring-System-Based-On-RaspberryPi/httpserver/SMSsending.c" "/home/pi/Desktop/Remote-Monitoring-System-Based-On-RaspberryPi/httpserver/CMakeFiles/RemoteMonit.dir/SMSsending.c.o"
+  "/home/pi/Desktop/Remote-Monitoring-System-Based-On-RaspberryPi/httpserver/http_work.c" "/home/pi/Desktop/Remote-Monitoring-System-Based-On-RaspberryPi/httpserver/CMakeFiles/RemoteMonit.dir/http_work.c.o"
+  "/home/pi/Desktop/Remote-Monitoring-System-Based-On-RaspberryPi/httpserver/server.c" "/home/pi/Desktop/Remote-Monitoring-System-Based-On-RaspberryPi/httpserver/CMakeFiles/RemoteMonit.dir/server.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Desktop/Remote-Monitoring-System-Based-On-RaspberryPi/httpserver/face_only_rpi.cpp" "/home/pi/Desktop/Remote-Monitoring-System-Based-On-RaspberryPi/httpserver/CMakeFiles/RemoteMonit.dir/face_only_rpi.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ httpserver/CMakeFiles/RemoteMonit.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RemoteMonit.dir/SMSsending.c.o"
+  "CMakeFiles/RemoteMonit.dir/face_only_rpi.cpp.o"
+  "CMakeFiles/RemoteMonit.dir/http_work.c.o"
+  "CMakeFiles/RemoteMonit.dir/server.c.o"
+  "RemoteMonit.pdb"
+  "RemoteMonit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/RemoteMonit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
